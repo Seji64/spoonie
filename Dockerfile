109fd0390b9a494129de87545224ca76131bd97b
Seji64 <seji@tihoda.de>
2024-01-06 23:08:41 +0100

    improve / fix docker cron schedule

--- Dockerfile
+++ Dockerfile
@@ -9,18 +9,17 @@ ENV TONIE_HOUSEHOLD=
 ENV DATA_PATH=/app/data
 ENV CREATIVE_TONIE=
 ENV PLAYLIST=
+ENV CRON="*/5 * * * *"
 
 WORKDIR /app
 COPY spoonie.py .
 COPY requirements.txt .
-COPY crontab /etc/cron.d/crontab
 COPY entrypoint.sh .
 
 RUN apt-get update && apt-get -y upgrade && \
     apt-get --no-install-suggests --no-install-recommends -y install git cron tini ffmpeg && \
     pip3 install --upgrade pip && \
     pip3 install -r requirements.txt && \
-    chmod 0644 /etc/cron.d/crontab && \
     chmod +x /app/entrypoint.sh
 
 ENTRYPOINT ["/usr/bin/tini", "--"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 printenv > /etc/environment
+echo "############### Installing Cron Job ###############"
+echo "${CRON} python3 /app/spoonie.py --spotify-user "$SPOTIFY_USERNAME" --spotify-password '${SPOTIFY_PASSWORD}' --tonie-username "$TONIE_USERNAME" --tonie-password '${TONIE_PASSWORD}' --tonie-household \"${TONIE_HOUSEHOLD}\" --creative-tonie \"${CREATIVE_TONIE}\" --playlist "$PLAYLIST" --data-path \"${DATA_PATH}\" > /proc/1/fd/1 2>/proc/1/fd/2" > /tmp/crontab.file
+crontab -i /tmp/crontab.file
+rm -f /tmp/crontab
+crontab -l
+echo "############### Cron Job installed ! ###############"
 echo "############### Starting initial run... ###############"
 python3 /app/spoonie.py --spotify-user "$SPOTIFY_USERNAME" --spotify-password "$SPOTIFY_PASSWORD" \
         --tonie-username "$TONIE_USERNAME" --tonie-password "$TONIE_PASSWORD" --tonie-household "$TONIE_HOUSEHOLD" \
         --creative-tonie "$CREATIVE_TONIE" --playlist "$PLAYLIST" --data-path "$DATA_PATH"
 echo "############### Initial run completed! ###############"
 echo "############### Starting CRON ###############"
-cron -f
+touch /var/log/cron.log
+cron
+tail -f /var/log/cron.log
